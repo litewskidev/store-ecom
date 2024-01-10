524a9a27eda2bea053899be25c770559f9e58cfb
Adrian Litewski <litewskidev@gmail.com>
2024-01-10 23:41:08 +0100

    new navbar

--- src/components/Elements/Navbar/Navbar.jsx
+++ src/components/Elements/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useRef } from 'react';
 import './Navbar.scss';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const dropdownBtnRef = useRef(null);
@@ -13,72 +14,96 @@ const Navbar = () => {
   };
 
   return(
-    <div className='navbar__wrapper'>
-      <nav className='navbar__nav'>
-        <div className='navbar__nav__logo'>
-          <h1>CULTURE</h1>
+    <div className='navbar'>
+      <div className='navbar__wrapper'>
+        <div className='navbar__body'>
+          <nav className='navbar__body__left'>
+            <ul className='navbar__items'>
+              <li className='navbar__item__menu'>
+                <button className='navbar__button'>
+                  <div className='navbar__button__up'></div>
+                  <div className='navbar__button__down'></div>
+                </button>
+              </li>
+              <li className='navbar__item__icon'>
+                <NavLink className='navbar__item__icon__wishlist' to='/profile/wishlist'>
+                  <img src={process.env.PUBLIC_URL + '/assets/icons/clock.svg'} alt='wishlist button' />
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <h1 className='navbar__body__logo'>CULTURE</h1>
+
+          <nav className='navbar__body__center'>
+            <ul className='navbar__items'>
+              <li className='navbar__item__link'>
+                <NavLink to='/shop/new-arrivals'>NEW ARRIVALS</NavLink>
+              </li>
+              <li className='navbar__item__link'>
+                <NavLink to='/shop/all-watches'>ALL WATCHES</NavLink>
+              </li>
+              <li className='navbar__item__link'>
+                <NavLink to='/watches/all-brands'>BRANDS</NavLink>
+              </li>
+              <li className='navbar__item__link'>
+                <div className='navbar__search__container'>
+                  <form className='navbar__search__form'>
+                    <div className='navbar__search__form__field'>
+                      <input type='text'></input>
+                      <label></label>
+                    </div>
+                    <button className='navbar__search__form__button'>
+                      <img src={process.env.PUBLIC_URL + '/assets/icons/search.svg'} alt='search button' />
+                    </button>
+                  </form>
+                </div>
+              </li>
+            </ul>
+          </nav>
+
+          <nav className='navbar__body__right'>
+            <ul className='navbar__items'>
+              <li className='navbar__item__icon tablet__only'>
+                <div className='navbar__search__container'>
+                  <form className='navbar__search__form'>
+                    <div className='navbar__search__form__field'>
+                      <input type='text'></input>
+                      <label></label>
+                    </div>
+                    <button className='navbar__search__form__button'>
+                      <img src={process.env.PUBLIC_URL + '/assets/icons/search.svg'} alt='search button' />
+                    </button>
+                  </form>
+                </div>
+              </li>
+              <li className='navbar__item__icon desktop__only'>
+                <NavLink className='navbar__item__icon__wishlist' to='/profile/wishlist'>
+                  <img src={process.env.PUBLIC_URL + '/assets/icons/clock.svg'} alt='wishlist button' />
+                </NavLink>
+              </li>
+              <li className='navbar__item__icon'>
+                <NavLink to='/profile'>
+                  <img src={process.env.PUBLIC_URL + '/assets/icons/user.svg'} alt='account button' />
+                </NavLink>
+              </li>
+              <li className='navbar__item__icon'>
+                <NavLink to='/cart'>
+                  <img src={process.env.PUBLIC_URL + '/assets/icons/cart.svg'} alt='cart button' />
+                </NavLink>
+              </li>
+              <li className='navbar__item__menu tablet__only'>
+                <button className='navbar__button'>
+                  <div className='navbar__button__up'></div>
+                  <div className='navbar__button__down'></div>
+                </button>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <div className='navbar__nav__controls'>
-          <div className='navbar__nav__controls__profile-btn'>
-            <img src={process.env.PUBLIC_URL + '/assets/icons/user.svg'} alt='profile button'/>
-          </div>
-          <div className='navbar__nav__controls__dropdown-btn' ref={dropdownBtnRef} onClick={toggleDropdown}>
-            <div className='navbar__nav__controls__dropdown-btn__up'></div>
-            <div className='navbar__nav__controls__dropdown-btn__down'></div>
-          </div>
-        </div>
-      </nav>
-      <nav className='navbar__menu' ref={dropdownRef}>
-        <div className='navbar__menu__list'>
-          <div className='navbar__menu__list__item'>
-            <div className='navbar__menu__list__item__title'>
-              <div className='navbar__menu__list__item__title__text'>
 
-              </div>
-              <div className='navbar__menu__list__item__title__icon'>
-
-              </div>
-            </div>
-            <div className='navbar__menu__list__item__content'>
-              <ul>
-
-              </ul>
-            </div>
-          </div>
-
-          <div className='navbar__menu__list__item'>
-            <div className='navbar__menu__list__item__title'>
-              <div className='navbar__menu__list__item__title__text'>
-
-              </div>
-              <div className='navbar__menu__list__item__title__icon'>
-
-              </div>
-            </div>
-            <div className='navbar__menu__list__item__content'>
-              <ul>
-
-              </ul>
-            </div>
-          </div>
-
-          <div className='navbar__menu__list__item'>
-            <div className='navbar__menu__list__item__title'>
-              <div className='navbar__menu__list__item__title__text'>
-
-              </div>
-              <div className='navbar__menu__list__item__title__icon'>
-
-              </div>
-            </div>
-            <div className='navbar__menu__list__item__content'>
-              <ul>
-
-              </ul>
-            </div>
-          </div>
-        </div>
-      </nav>
+        <div className='navbar__footer'></div>
+      </div>
     </div>
   );
 };
