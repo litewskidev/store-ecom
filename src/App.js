b37411566d2208aa140c8a84c5e236fe691167c2
Adrian Litewski <litewskidev@gmail.com>
2024-01-25 23:55:15 +0100

    add site loader

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import Loading from './components/Elements/Loading/Loading.jsx';
 import Navbar from './components/Elements/Navbar/Navbar.jsx';
 import Newsletter from './components/Elements/Newsletter/Newsletter.jsx';
 import Footer from './components/Elements/Footer/Footer.jsx';
 import './styles/global.scss';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setLoading(false);
+    };
+
+    window.addEventListener('load', handleLoad);
+
+    return () => {
+      window.removeEventListener('load', handleLoad);
+    };
+  }, []);
+
   return(
-    <main id='main'>
-      <Navbar />
-      <div>
-        <Outlet />
+    loading ? (
+      <div style={{ height: '100svh', display: 'grid', placeSelf: 'center'  }}>
+        <Loading />
       </div>
-      <Newsletter />
-      <Footer />
-    </main>
+    ) : (
+      <main id='main'>
+        <Navbar />
+        <div>
+          <Outlet />
+        </div>
+        <Newsletter />
+        <Footer />
+      </main>
+    )
   );
 };
 
