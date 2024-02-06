3c36c1f9083c2f1b8436f81785caa8b7cef8882e
Adrian Litewski <litewskidev@gmail.com>
2024-02-06 01:23:13 +0100

    add stickyHalf comp

---
+++ src/components/Elements/StickyHalf/StickyHalf.jsx
@@ -0,0 +1,35 @@
+import { NavLink } from 'react-router-dom';
+import './StickyHalf.scss'
+
+const StickyHalf = () => {
+  return(
+    <div className='stickyHalf__wrapper'>
+      <div className='stickyHalf__container'>
+        <div className='stickyHalf__info'>
+          <NavLink to='/brands/breitling' className='stickyHalf__info__inner'>
+            <h3>SINCE 1884</h3>
+            <p>Discover the epitome of Swiss craftsmanship and cutting-edge design with Breitling, a renowned brand synonymous with precision and luxury in the world of watches.</p>
+            <NavLink to='/brands/breitling'>SHOP BREITLING</NavLink>
+          </NavLink>
+        </div>
+        <div className='stickyHalf__image'>
+          <img src={process.env.PUBLIC_URL + '/assets/images/img_10.webp'} alt='' />
+        </div>
+      </div>
+      <div className='stickyHalf__container half__bottom'>
+        <div className='stickyHalf__image'>
+          <img src={process.env.PUBLIC_URL + '/assets/images/img_11.webp'} alt='' />
+        </div>
+        <div className='stickyHalf__info'>
+          <NavLink to='/brands/rolex' className='stickyHalf__info__inner'>
+            <h3>SINCE 1905</h3>
+            <p>Indulge in the epitome of luxury and prestige with Rolex, a brand synonymous with excellence, innovation, and enduring style.</p>
+            <NavLink to='/brands/rolex'>SHOP ROLEX</NavLink>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StickyHalf;
--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Featured from "../../Elements/Featured/Featured.jsx";
 import Hero from "../../Elements/Hero/Hero.jsx";
 import HeroAbout from "../../Elements/HeroAbout/HeroAbout.jsx";
 import NewArrivals from "../../Elements/NewArrivals/NewArrivals.jsx";
+import StickyHalf from "../../Elements/StickyHalf/StickyHalf.jsx";
 import './Home.scss';
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <NewArrivals />
       <Featured />
       <HeroAbout />
+      <StickyHalf />
     </section>
   );
 };
